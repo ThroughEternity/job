690f37438e5fb28f67f5c37c8efe0669659149c7
ThroughEternity <eternity1666@gmail.com>
2022-08-04 20:49:12 +0400

    add name

--- menu.php
+++ menu.php
@@ -3,99 +3,99 @@
                     <div class="row d-flex row-cols-1 row-cols-lg-3 row-cols-xl-4  ">
                         <div class=" col d-flex justify-content-center ">
                             <div class=" col-12  row "> <img class="img-fluid " src="./img/image 11.svg" alt="картинка ">
-                           <h2>Увлажнитель воздуха  STARWIND SHC1322, 3л, ,белый</h2> 
-                            <h1>1650р</h1>
+                           <span class="descript">Увлажнитель воздуха STARWIND SHC1322, 3л, белый</span> 
+                            <p class="price">1650 ₽</p>
                         </div>
                         </div>
                       
                         <div class=" col d-flex justify-content-center ">
                             <div class=" col-12  row "> <img class="img-fluid " src="./img/image 11 (1).svg" alt="картинка ">
-                           <h2>Увлажнитель воздуха  STARWIND SHC1322, 3л, ,белый</h2> 
-                            <h1>1650р</h1>
+                           <span class="descript" >Триммер PHILIPS HC3521/15 серебристый/черный</span> 
+                            <p class="price">2290 ₽</p>
                         </div>
                         </div>
                         <div class=" col d-flex justify-content-center ">
                             <div class=" col-12  row "> <img class="img-fluid " src="./img/image 11 (2).svg" alt="картинка ">
-                           <h2>Увлажнитель воздуха  STARWIND SHC1322, 3л, ,белый</h2> 
-                            <h1>1650р</h1>
+                           <span class="descript">Фитнес-трекер HONOR Band 5 CRS-B19S, 0.95", розовый</span> 
+                            <p class="price">2390 ₽</p>
                         </div>
                         </div>
                         <div class=" col d-flex justify-content-center ">
                             <div class=" col-12  row "> <img class="img-fluid " src="./img/image 11 (3).svg" alt="картинка ">
-                           <h2>Увлажнитель воздуха  STARWIND SHC1322, 3л, ,белый</h2> 
-                            <h1>1650р</h1>
+                           <span class="descript">Мышь A4TECH Bloody V3, игровая, оптическая, проводная, USB, черный</span> 
+                            <p class="price">960 ₽</p>
                         </div>
                         </div>
                         <div class=" col d-flex justify-content-center ">
                             <div class=" col-12  row "> <img class="img-fluid " src="./img/image 11 (4).svg" alt="картинка ">
-                           <h2>Увлажнитель воздуха  STARWIND SHC1322, 3л, ,белый</h2> 
-                            <h1>1650р</h1>
+                           <span class="descript">Фитнес-трекер HONOR Band 5 CRS-B19S, 0.95", черный</span> 
+                            <p class="price">2390 ₽</p>
                         </div>
                         </div>
                         <div class=" col d-flex justify-content-center ">
                             <div class=" col-12  row "> <img class="img-fluid " src="./img/image 11 (5).svg" alt="картинка ">
-                           <h2>Увлажнитель воздуха  STARWIND SHC1322, 3л, ,белый</h2> 
-                            <h1>1650р</h1>
+                           <span class="descript">Пылесос SINBO SVC 3497, 2500Вт, синий/серый</span> 
+                            <p class="price">5670 ₽</p>
                         </div>
                         </div>
                         <div class=" col d-flex justify-content-center ">
                             <div class=" col-12  row "> <img class="img-fluid " src="./img/image 11 (6).svg" alt="картинка ">
-                           <h2>Увлажнитель воздуха  STARWIND SHC1322, 3л, ,белый</h2> 
-                            <h1>1650р</h1>
+                           <span class="descript">Планшет DIGMA Optima 7 Z800 Android 10.0 серебристый</span> 
+                            <p class="price">9490 ₽</p>
                         </div>
                         </div>
                         <div class=" col d-flex justify-content-center ">
                             <div class=" col-12  row "> <img class="img-fluid " src="./img/image 11 (7).svg" alt="картинка ">
-                           <h2>Увлажнитель воздуха  STARWIND SHC1322, 3л, ,белый</h2> 
-                            <h1>1650р</h1>
+                           <span class="descript">Монитор игровой ACER Nitro RG241YPbiipx 23.8" черный</span> 
+                            <p class="price">16 800 ₽</p>
                         </div>
                         </div>
                         <div class=" col d-flex justify-content-center ">
                             <div class=" col-12  row "> <img class="img-fluid " src="./img/image 11 (8).svg" alt="картинка ">
-                           <h2>Увлажнитель воздуха  STARWIND SHC1322, 3л, ,белый</h2> 
-                            <h1>1650р</h1>
+                           <span class="descript">Экшн-камера DIGMA DiCam 310 4K, WiFi, черный</span> 
+                            <p class="price">2290 ₽</p>
                         </div>
                         </div>
                         <div class=" col d-flex justify-content-center ">
                             <div class=" col-12  row "> <img class="img-fluid " src="./img/image 11 (9).svg" alt="картинка ">
-                           <h2>Увлажнитель воздуха  STARWIND SHC1322, 3л, ,белый</h2> 
-                            <h1>1650р</h1>
+                           <span class="descript">Умная колонка ЯНДЕКС c голосовым помощником Алисой, серебристый</span> 
+                            <p class="price">5670 ₽</p>
                         </div>
                         </div>
                         <div class=" col d-flex justify-content-center ">
                             <div class=" col-12  row "> <img class="img-fluid " src="./img/image 11 (10).svg" alt="картинка ">
-                           <h2>Увлажнитель воздуха  STARWIND SHC1322, 3л, ,белый</h2> 
-                            <h1>1650р</h1>
+                           <span class="descript">Квадрокоптер DJI Mini 2 MT2PD Fly More Combo с камерой, серый</span> 
+                            <p class="price">60 990 ₽</p>
                         </div>
                         </div>
                         <div class=" col d-flex justify-content-center ">
                             <div class=" col-12  row "> <img class="img-fluid " src="./img/image 11 (11).svg" alt="картинка ">
-                           <h2>Увлажнитель воздуха  STARWIND SHC1322, 3л, ,белый</h2> 
-                            <h1>1650р</h1>
+                           <span class="descript">Шлем виртуальной реальности HTC Vive PRO Eye EEA, черный/синий</span> 
+                            <p class="price">11 960 ₽</p>
                         </div>
                         </div>
                         <div class=" col d-flex justify-content-center ">
                             <div class=" col-12  row "> <img class="img-fluid " src="./img/image 11 (12).svg" alt="картинка ">
-                           <h2>Увлажнитель воздуха  STARWIND SHC1322, 3л, ,белый</h2> 
-                            <h1>1650р</h1>
+                           <span class="descript">МФУ лазерный CANON i-Sensys MF445dw, A4, лазерный, черный</span> 
+                            <p class="price">35 310 ₽</p>
                         </div>
                         </div>
                         <div class=" col d-flex justify-content-center ">
                             <div class=" col-12  row "> <img class="img-fluid " src="./img/image 11 (13).svg" alt="картинка ">
-                           <h2>Увлажнитель воздуха  STARWIND SHC1322, 3л, ,белый</h2> 
-                            <h1>1650р</h1>
+                           <span class="descript">Смарт-часы AMAZFIT Bip U, 1.43", зеленый / зеленый</span> 
+                            <p class="price">4490 ₽</p>
                         </div>
                         </div>
                         <div class=" col d-flex justify-content-center ">
                             <div class=" col-12  row "> <img class="img-fluid " src="./img/image 11 (14).svg" alt="картинка ">
-                           <h2>Увлажнитель воздуха  STARWIND SHC1322, 3л, ,белый</h2> 
-                            <h1>1650р</h1>
+                           <span class="descript">Кофемашина PHILIPS EP1224/00, серый/черный</span> 
+                            <p class="price">29 990 ₽</p>
                         </div>
                         </div>
                         <div class=" col d-flex justify-content-center ">
                             <div class=" col-12  row "> <img class="img-fluid " src="./img/image 11 (15).svg" alt="картинка ">
-                           <h2>Увлажнитель воздуха  STARWIND SHC1322, 3л, ,белый</h2> 
-                            <h1>1650р</h1>
+                           <span class="descript">Гироскутер MIZAR MZ10,5CN, 10.5", карбон</span> 
+                            <p class="price">12 990 ₽</p>
                         </div>
                         </div>
                         
