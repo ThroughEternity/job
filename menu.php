391f0028d61bb981119d7747f31eb8e1dfa3ad20
ThroughEternity <eternity1666@gmail.com>
2022-08-04 20:16:00 +0400

    add munu

---
+++ menu.php
@@ -0,0 +1,145 @@
+<section class="soft">
+                <div class="container">
+                    <div class="row d-flex row-cols-1 row-cols-lg-3 row-cols-xl-4  ">
+                        <div class=" col d-flex justify-content-center ">
+                            <div class=" col-12  row "> <img class="img-fluid " src="./img/image 11.svg" alt="картинка ">
+                           <h2>Увлажнитель воздуха  STARWIND SHC1322, 3л, ,белый</h2> 
+                            <h1>1650р</h1>
+                        </div>
+                        </div>
+                      
+                        <div class=" col d-flex justify-content-center ">
+                            <div class=" col-12  row "> <img class="img-fluid " src="./img/image 11 (1).svg" alt="картинка ">
+                           <h2>Увлажнитель воздуха  STARWIND SHC1322, 3л, ,белый</h2> 
+                            <h1>1650р</h1>
+                        </div>
+                        </div>
+                        <div class=" col d-flex justify-content-center ">
+                            <div class=" col-12  row "> <img class="img-fluid " src="./img/image 11 (2).svg" alt="картинка ">
+                           <h2>Увлажнитель воздуха  STARWIND SHC1322, 3л, ,белый</h2> 
+                            <h1>1650р</h1>
+                        </div>
+                        </div>
+                        <div class=" col d-flex justify-content-center ">
+                            <div class=" col-12  row "> <img class="img-fluid " src="./img/image 11 (3).svg" alt="картинка ">
+                           <h2>Увлажнитель воздуха  STARWIND SHC1322, 3л, ,белый</h2> 
+                            <h1>1650р</h1>
+                        </div>
+                        </div>
+                        <div class=" col d-flex justify-content-center ">
+                            <div class=" col-12  row "> <img class="img-fluid " src="./img/image 11 (4).svg" alt="картинка ">
+                           <h2>Увлажнитель воздуха  STARWIND SHC1322, 3л, ,белый</h2> 
+                            <h1>1650р</h1>
+                        </div>
+                        </div>
+                        <div class=" col d-flex justify-content-center ">
+                            <div class=" col-12  row "> <img class="img-fluid " src="./img/image 11 (5).svg" alt="картинка ">
+                           <h2>Увлажнитель воздуха  STARWIND SHC1322, 3л, ,белый</h2> 
+                            <h1>1650р</h1>
+                        </div>
+                        </div>
+                        <div class=" col d-flex justify-content-center ">
+                            <div class=" col-12  row "> <img class="img-fluid " src="./img/image 11 (6).svg" alt="картинка ">
+                           <h2>Увлажнитель воздуха  STARWIND SHC1322, 3л, ,белый</h2> 
+                            <h1>1650р</h1>
+                        </div>
+                        </div>
+                        <div class=" col d-flex justify-content-center ">
+                            <div class=" col-12  row "> <img class="img-fluid " src="./img/image 11 (7).svg" alt="картинка ">
+                           <h2>Увлажнитель воздуха  STARWIND SHC1322, 3л, ,белый</h2> 
+                            <h1>1650р</h1>
+                        </div>
+                        </div>
+                        <div class=" col d-flex justify-content-center ">
+                            <div class=" col-12  row "> <img class="img-fluid " src="./img/image 11 (8).svg" alt="картинка ">
+                           <h2>Увлажнитель воздуха  STARWIND SHC1322, 3л, ,белый</h2> 
+                            <h1>1650р</h1>
+                        </div>
+                        </div>
+                        <div class=" col d-flex justify-content-center ">
+                            <div class=" col-12  row "> <img class="img-fluid " src="./img/image 11 (9).svg" alt="картинка ">
+                           <h2>Увлажнитель воздуха  STARWIND SHC1322, 3л, ,белый</h2> 
+                            <h1>1650р</h1>
+                        </div>
+                        </div>
+                        <div class=" col d-flex justify-content-center ">
+                            <div class=" col-12  row "> <img class="img-fluid " src="./img/image 11 (10).svg" alt="картинка ">
+                           <h2>Увлажнитель воздуха  STARWIND SHC1322, 3л, ,белый</h2> 
+                            <h1>1650р</h1>
+                        </div>
+                        </div>
+                        <div class=" col d-flex justify-content-center ">
+                            <div class=" col-12  row "> <img class="img-fluid " src="./img/image 11 (11).svg" alt="картинка ">
+                           <h2>Увлажнитель воздуха  STARWIND SHC1322, 3л, ,белый</h2> 
+                            <h1>1650р</h1>
+                        </div>
+                        </div>
+                        <div class=" col d-flex justify-content-center ">
+                            <div class=" col-12  row "> <img class="img-fluid " src="./img/image 11 (12).svg" alt="картинка ">
+                           <h2>Увлажнитель воздуха  STARWIND SHC1322, 3л, ,белый</h2> 
+                            <h1>1650р</h1>
+                        </div>
+                        </div>
+                        <div class=" col d-flex justify-content-center ">
+                            <div class=" col-12  row "> <img class="img-fluid " src="./img/image 11 (13).svg" alt="картинка ">
+                           <h2>Увлажнитель воздуха  STARWIND SHC1322, 3л, ,белый</h2> 
+                            <h1>1650р</h1>
+                        </div>
+                        </div>
+                        <div class=" col d-flex justify-content-center ">
+                            <div class=" col-12  row "> <img class="img-fluid " src="./img/image 11 (14).svg" alt="картинка ">
+                           <h2>Увлажнитель воздуха  STARWIND SHC1322, 3л, ,белый</h2> 
+                            <h1>1650р</h1>
+                        </div>
+                        </div>
+                        <div class=" col d-flex justify-content-center ">
+                            <div class=" col-12  row "> <img class="img-fluid " src="./img/image 11 (15).svg" alt="картинка ">
+                           <h2>Увлажнитель воздуха  STARWIND SHC1322, 3л, ,белый</h2> 
+                            <h1>1650р</h1>
+                        </div>
+                        </div>
+                        
+                      
+                       
+                      
+
+
+
+
+
+
+
+                        <div class=" col  d-flex justify-content-center"><img class="img-fluid " src="./img/image 11 (1).svg"
+                                alt="картинка "></div>
+                        <div class=" col  d-flex justify-content-center"><img class="img-fluid " src="./img/image 11 (2).svg "
+                                alt="картинка "></div>
+                        <div class=" col  d-flex justify-content-center"><img class="img-fluid " src="./img/image 11 (3).svg "
+                                alt="картинка "></div>
+                        <div class=" col d-flex justify-content-center"><img class="img-fluid " src="./img/image 11 (4).svg "
+                                alt="картинка "></div>
+                        <div class=" col  d-flex justify-content-center"><img class="img-fluid " src="./img/image 11 (5).svg "
+                                alt="картинка "></div>
+                        <div class=" col d-flex justify-content-center"><img class="img-fluid " src="./img/image 11 (6).svg "
+                                alt="картинка "></div>
+                        <div class=" col  d-flex justify-content-center"><img class="img-fluid " src="./img/image 11 (7).svg "
+                                alt="картинка "></div>
+                        <div class=" col  d-flex justify-content-center"><img class="img-fluid " src="./img/image 11 (8).svg "
+                                alt="картинка "></div>
+                        <div class=" col  d-flex justify-content-center"><img class="img-fluid " src="./img/image 11 (9).svg "
+                                alt="картинка "></div>
+                        <div class=" col  d-flex justify-content-center"><img class="img-fluid " src="./img/image 11 (10).svg "
+                                alt="картинка "></div>
+                        <div class=" col  d-flex justify-content-center"><img class="img-fluid " src="./img/image 11 (11).svg "
+                                alt="картинка "></div>
+                        <div class=" col  d-flex justify-content-center"><img class="img-fluid " src="./img/image 11 (12).svg "
+                                alt="картинка "></div>
+                        <div class=" col  d-flex justify-content-center"><img class="img-fluid " src="./img/image 11 (13).svg "
+                                alt="картинка "></div>
+                        <div class=" col  d-flex justify-content-center"><img class="img-fluid " src="./img/image 11 (14).svg "
+                                alt="картинка "></div>
+                        <div class=" col d-flex justify-content-center"><img class="img-fluid " src="./img/image 11 (15).svg "
+                                alt="картинка "></div>
+         
+                    </div>   
+              </div>
+         </section>
